d3ea62d57a65316c1b6a5e4b6334fb2be4630333
David Plumpton <david.plumpton@gmail.com>
2011-05-01 14:01:33 +1200

    Multiple players in the ship

--- sa.clj
+++ sa.clj
@@ -7,8 +7,10 @@
 
 (defrecord Ship [rooms])
 
-(defn create-initial-ship []
-  (Ship. {:red-up [] :white-up [:player1] :blue-up []}))
+(defn create-initial-ship [num-players]
+  (Ship. {:red-up []
+	  :white-up (vec (map (fn [num] (keyword (str "player" num))) (range 1 (inc num-players))))
+	  :blue-up []}))
 
 (defn find-player [ship player]
   (first (first
@@ -19,5 +21,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (deftest ship-structure
-  (let [ship (create-initial-ship)]
-    (is (= :white-up (find-player ship :player1)))))
+  (let [ship (create-initial-ship 4)]
+    (is (= :white-up (find-player ship :player1)))
+    (is (= :white-up (find-player ship :player4)))))
