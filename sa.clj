edd0094f49d589e52b72ed19bbe04ad5d92ccd04
David Plumpton <david.plumpton@gmail.com>
2012-04-12 08:00:59 +1200

    Change a few things about how threats are represented

--- sa.clj
+++ sa.clj
@@ -19,6 +19,19 @@
      [:z :_ :y :_ :_, :_ :y :_ :_ :x, :_ :_ :_ :_ :_]
      [:z :_ :_ :_ :y, :_ :_ :y :_ :_, :_ :x :_ :_ :_ :_]])
 
+(def threats
+  {:e1-01 [:pulse-ball 5 1 2 2 4 [:all-zones 1] [:all-zones 1] [:all-zones 2]]
+   :e1-02 [:destroyer 5 2 2 2 4 [:attack 1] [:attack 1] [:attack 2] :double-damage]
+   :e1-03 [:steath-fighter 4 2 4 2 4 [:reveal] [:attack 2] [:attack 2] :stealth-until-reveal]
+   :e1-04 [:energy-cloud 5 3 2 2 4 [:drain-all-shields] [:other-zones 2] [:other-zones 2] :no-shield-if-pulse-cannon-hits]
+   :e1-05 [:gunship 5 2 2 2 4 [:attack 2] [:attack 2] [:attack 3]]
+   :e1-06 [:cryoshield-fighter 4 1 3 2 4 [:attack 1] [:attack 1] [:attack 2]]
+   :e1-07 [:fighter 5 2 3 2 4 [:attack 1] [:attack 1] [:attack 2]]
+   :e1-08 [:armored-grappler 4 3 2 2 4 [:attack 1] [:repair 1] [:attack 4]]
+   :e1-09 [:amoeba 8 0 2 2 4 [:repair 2] [:repair 2] [:attack 5] :no-rockets]
+   :e1-10 [:meteroid 5 0 5 [] [] [:attack-hit-points] :no-rockets]
+   :se1-01 [:frigate 7 2 2 4 8 [:attack 2] [:attack 3] [:attack 4]]})
+
 (defn create-initial-ship
   "Create the starting position, specifying the number of players"
   ([num-players]
@@ -35,8 +48,8 @@
   "Find the room a player is in"
   [ship player]
   (ffirst (filter
-	   (fn [[room contents]] (some #{player} contents))
-	   (:rooms ship))))
+           (fn [[room contents]] (some #{player} contents))
+           (:rooms ship))))
 
 (defn- move-grav-lift
   "Determine which room the grav-lift leads to"
@@ -67,11 +80,11 @@
   "Update the ship to move a player in some direction"
   [ship player direction]
   (let [ship-rooms (:rooms ship)
-	position (find-player ship player)
-	removed-rooms (update-in ship-rooms [position] #(remove #{player} %))
-	destination-room (which-room position direction)
-	destination-contents (destination-room removed-rooms)
-	updated-rooms (assoc removed-rooms destination-room (conj destination-contents player))]
+        position (find-player ship player)
+        removed-rooms (update-in ship-rooms [position] #(remove #{player} %))
+        destination-room (which-room position direction)
+        destination-contents (destination-room removed-rooms)
+        updated-rooms (assoc removed-rooms destination-room (conj destination-contents player))]
     (assoc ship :rooms updated-rooms)))
 
 (defn create-threat [number type zone distance id]
@@ -104,18 +117,24 @@
   ([turns step player]
      (get-in turns [step player])))
 
-(defn play-step
+(defn player-turn
   "Play all the turns at a certain step"
   [ship turns step]
   (reduce
    (fn [ship turn] (move-player ship (turn-player turn) (turn-direction turn)))
    ship (get-turn turns step)))
 
+(defn threats-move
+  "All threats move"
+  [ship]
+    (assoc ship :threats
+      (map (fn [threat] (assoc threat :distance 7)) (:threats ship))))
+
 (defn- pp-rooms [ship rooms]
   (apply str (for [room rooms]
-	       (let [player-str (apply str (for [player (room (:rooms ship))] (.substring (name player) 6)))
-		     room-str (str player-str (.substring "      " (count player-str)))]
-		     (str "| " room-str)))))
+               (let [player-str (apply str (for [player (room (:rooms ship))] (.substring (name player) 6)))
+                     room-str (str player-str (.substring "      " (count player-str)))]
+                     (str "| " room-str)))))
 
 (defn- trajectory-element-str
   [trajectory index length]
@@ -155,7 +174,7 @@
     divider
     (pp-rooms ship [:red-down :white-down :blue-down]) "|\n"
     divider
-    (pp-internal-trajectory ship))))
+    (pp-internal-trajectory ship) "\n")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -167,7 +186,7 @@
 
 (deftest player-move
   (let [ship (create-initial-ship 4)
-	ship-with-player2-left (move-player ship :player2 :left)]
+        ship-with-player2-left (move-player ship :player2 :left)]
     (is (= :red-up (find-player ship-with-player2-left :player2)))
     (is (= :white-up (find-player ship-with-player2-left :player3)))
     (is (= :white-up (find-player (move-player ship-with-player2-left :player2 :right) :player3)))))
@@ -187,10 +206,10 @@
 
 (deftest threats
   (let [ship (create-initial-ship 4)
-	threat1 (create-threat 3 :energy-cloud :blue 8 :e1-04)
-	threat2 (create-threat 5 :fighter :red 9 :e1-07)
-	threats [threat1 threat2]
-	ship-with-threats (reduce add-threat ship threats)]
+        threat1 (create-threat 3 :energy-cloud :blue 8 :e1-04)
+        threat2 (create-threat 5 :fighter :red 9 :e1-07)
+        threats [threat1 threat2]
+        ship-with-threats (reduce add-threat ship threats)]
     (is (= 0 (count (:threats ship))))
     (is (= 2 (count (:threats ship-with-threats))))
     (is (= :energy-cloud (:type (first (:threats ship-with-threats)))))
@@ -206,16 +225,16 @@
 
 (deftest add-turn-should-work-for-multiple-steps
   (let [ship (create-initial-ship 4)
-	turns (create-sample-turns)]
+        turns (create-sample-turns)]
     (is (= :left (get-turn turns 1 :player1)))
     (is (= :right (get-turn turns 1 :player2)))
     (is (= :updown (get-turn turns 2 :player1)))
     (is (= :right (get-turn turns 3 :player1)))))
 
-(deftest play-step-should-work
+(deftest player-turn-should-work
   (let [ship (create-initial-ship 4)
-	turns (create-sample-turns)
-	ship-after-step-1 (play-step ship turns 1)]
+        turns (create-sample-turns)
+        ship-after-step-1 (player-turn ship turns 1)]
     (is (= :red-up (find-player ship-after-step-1 :player1)))
     (is (= :blue-up (find-player ship-after-step-1 :player2)))
     (is (= :white-up (find-player ship-after-step-1 :player3)))))
@@ -223,3 +242,11 @@
 (deftest trajectories-should-default
   (let [ship (create-initial-ship 4)]
     (is (= (nth trajectories 0) (get-in ship [:trajectories :red])))))
+
+(deftest threats-should-move
+  (let [ship (create-initial-ship 4)
+        threat (create-threat 1 :fighter :red 10 :e1-07)
+        ship-with-threat (add-threat ship threat)
+        updated (threats-move ship-with-threat)]
+    (is (= 10 (:distance threat)))
+    (is (= 7 (:distance (first (:threats updated)))))))
