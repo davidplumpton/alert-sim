45fa262bdb33786c4eb228b68e442429affc2a9c
David Plumpton <david.plumpton@gmail.com>
2013-01-27 15:09:41 +1300

    Start threat attacks

--- sa.clj
+++ sa.clj
@@ -96,11 +96,20 @@
         track (:track ((:track threat) game))
         position (:position threat)
         velocity (:velocity threat)]
-    (assoc-in game [threat-id :position] (- position velocity))))
+    (if (> position 8)
+      (assoc-in game [threat-id :position] (- position velocity))
+      (assoc-in (assoc-in game [threat-id :position] (- position velocity)) [:red-shield :power] 0))))
+
+(defn threat-attack
+  [game threat-id action]
+  (assoc-in game [:red-shield :power] 0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def game4 (create-game 4))
+
+(defn- create-game-with-threat []
+  (add-threat game4 :e1-07 :red-track 2))
 
 (deftest create-game-should-have-players
   (are [x y] (= x y)
@@ -182,10 +191,25 @@
       (dec (count (:track track))) (:position threat)))) 
 
 (deftest threat-advance-should-work
-  (let [with-threat (add-threat game4 :e1-07 :red-track 2)
+  (let [with-threat (create-game-with-threat)
         threat-before (:e1-07 with-threat)
-        after-1-advance (advance-threat with-threat :e1-07)
-        threat-after-1 (:e1-07 after-1-advance)]
+        after-advance-1 (advance-threat with-threat :e1-07)
+        threat-after-1 (:e1-07 after-advance-1)
+        after-advance-2 (advance-threat after-advance-1 :e1-07)
+        threat-after-2 (:e1-07 after-advance-2)]
     (are [x y] (= x y)
       9 (:position threat-before)
-      6 (:position threat-after-1))))
+      6 (:position threat-after-1)
+      3 (:position threat-after-2))))
+
+(deftest threat-action-should-work
+  (let [with-threat (create-game-with-threat)
+        after-advance-1 (advance-threat with-threat :e1-07)
+        after-advance-2 (advance-threat after-advance-1 :e1-07)]
+    (are [x y] (= x y)
+      1 (:power (:red-shield after-advance-1))
+      1 (:power (:white-shield after-advance-1))
+      1 (:power (:blue-shield after-advance-1))
+      0 (:power (:red-shield after-advance-2))
+      1 (:power (:white-shield after-advance-2))
+      1 (:power (:blue-shield after-advance-2)))))
